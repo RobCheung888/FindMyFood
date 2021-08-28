2dcb5933e4ae08f77f3187b47566821bb091239d
Rob Cheung <22031605+RobCheung888@users.noreply.github.com>
2021-08-27 22:55:01 -0400

    Co-authored-by: cakkomilk <cakkomilk@gmail.com>

--- js/signin.js
+++ js/signin.js
@@ -1,1 +1,15 @@
-document.getElementById("signinbtn").innerHTML = localStorage.getItem("lastname");
+function refreshSignin() {
+if (localStorage.getItem("lastname")) {
+    document.getElementById("signintext").innerHTML = 'welcome, ' + localStorage.getItem("lastname");
+    document.getElementById("signinbtn").style.display = 'none';
+    document.getElementById("addrestbtn").style.display = 'block';
+    document.getElementById("signoutbtn").style.display = 'block';
+} else {
+    document.getElementById("signintext").innerHTML = 'please sign in';
+    document.getElementById("signinbtn").style.display = 'block';
+    document.getElementById("addrestbtn").style.display = 'none';
+    document.getElementById("signoutbtn").style.display = 'none';
+}
+}
+
+refreshSignin()
